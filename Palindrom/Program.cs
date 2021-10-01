10fac11cf904946df6783aa5a3489b49b5d437d6
Alfons Stoltz <alfons.stoltz@learnet.se>
2021-10-01 12:49:42 +0200

    Add project files.

---
+++ Palindrom/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ifinput = "";
+            string input;
+            Console.WriteLine("Är Palindrom?");
+            input = Console.ReadLine();
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                ifinput = ifinput + input[i];
+            }
+            if (input == ifinput)
+            {
+                Console.WriteLine("Ja\n" + ifinput);
+            }
+            else Console.WriteLine("Nej\n" + ifinput);
+        }
+    }
+}
